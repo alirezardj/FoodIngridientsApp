06767aea0352963edea912b112ea2df369a38137
alirezardj <alirezardj95@gmail.com>
2024-04-17 20:43:24 +0330

    Fix ESLint warnings in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-//import logo from "./logo.svg";
 import "./App.css";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-//import Navbar from "./Components/Navbar";
 import Meals from "./Components/Meals";
-//import Search from "./Components/Search";
 import { useGlobalContext } from "./context";
 import MyModal from "./Components/MyModal";
 
